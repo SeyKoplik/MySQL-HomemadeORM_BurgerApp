a55bb2a0731f381f46e459d1ef94d3f44c7f7e30
Seychelle Koplik <sey.koplik@gmail.com>
2020-10-07 17:58:03 -0400

    did some pseudo coding on orm file

--- config/orm.js
+++ config/orm.js
@@ -3,13 +3,60 @@ const connection = require('/connection.js');
 
 
 // create the methose that will execute the necessary MySQL commands in the controllers. These are the methodes you will need to use in order to retrieve and store data in your database
-const ORMObject = 
+const orm = {
+    selectAll: function (tableInput, callback) {
+        const queryString = "SELECT * FROM ? ";
+        connection.query(queryString, [tableInput], function (err, result) {
+            if (err) throw err;
 
+            callback(result);
+        });
+    },
 
-selectAll();
-insertOne();
-updateOne();
+    insertOne: function (table, cols, vals, cb) {
+        const queryString = "INSERT INTO " + table;
 
+        //================
+        //how to get enter the kind of burger into the database
+        //=== THIS IS CATS APP
+        // queryString += " (";
+        // queryString += cols.toString();
+        // queryString += ") ";
+        // queryString += "VALUES (";
+        // queryString += printQuestionMarks(vals.length);
+        // queryString += ") ";
 
+        // console.log(queryString);
 
-module.exports = ORMobject
+        connection.query(queryString, vals, function (err, result) {
+            if (err) throw err;
+
+            cb(result);
+        });
+    },
+
+    updateOne: function (table, objColVals, condition, cb) {
+        const queryString = "UPDATE " + table;
+
+        //================
+        //how to get enter the kind of burger into the database
+        //=== THIS IS CATS APP
+
+        // queryString += " SET ";
+        // queryString += objToSql(objColVals);
+        // queryString += " WHERE ";
+        // queryString += condition;
+
+        // console.log(queryString);
+
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
